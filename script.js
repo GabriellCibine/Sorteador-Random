0e55d81632bb7c9760fea391070a0cb79ee19506
Gabriellcibine <gabriellcibine@gmail.com>
2025-05-14 14:29:26 -0300

    Atualização-caixa-de-entrada-valor-minimo-e-maximo

--- script.js
+++ script.js
@@ -5,10 +5,16 @@ const sortear = document.querySelector("button")
 function generatenumber() {
     const numbermin = Math.ceil(document.querySelector(".min").value)
     const numbermax = Math.floor(document.querySelector(".max").value)
-    
-    const result = Math.floor(Math.random() * (numbermax - numbermin + 1)) + numbermin;
+
+    if( numbermin >= numbermax){
+        alert("O valor minimo tem que ser MENOR que o valor MÁXIMO!")
+    } else {
+        const result = Math.floor(Math.random() * (numbermax - numbermin + 1)) + numbermin;
 
     alert(result)
+    }
+    
+    
 }
 
 
